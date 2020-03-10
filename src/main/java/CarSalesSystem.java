7159daf486a19c6234667a7471cd165a36ffa140
Eduard Maura i Puig <edu.maura.puig@gmail.com>
2020-03-10 20:26:20 +0100

    Use logging instead of console outputs as debugging tool

--- src/main/java/CarSalesSystem.java
+++ src/main/java/CarSalesSystem.java
@@ -5,6 +5,8 @@ import java.awt.*;
 import java.awt.event.*;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static java.awt.Font.BOLD;
 import static java.awt.Font.PLAIN;
@@ -83,6 +85,8 @@ public class CarSalesSystem extends JFrame implements ActionListener, ComponentL
 	 */
 	public static final int AVERAGE_AGE = 4;
 
+	private final transient Logger logger = Logger.getLogger(this.getClass().getName());
+
 	private String file;
 	private AboutDialog aboutDlg;
 	private boolean carsUpdated = false;
@@ -124,18 +128,18 @@ public class CarSalesSystem extends JFrame implements ActionListener, ComponentL
 		}
 		catch (java.io.FileNotFoundException exp)
 		{
-			System.out.println("The data file, 'cars.dat' doesn't exist. Plase create an empty file named 'cars.dat'");
+			logger.log(Level.WARNING, "The data file, 'cars.dat' doesn't exist. Plase create an empty file named 'cars.dat'");
 			System.exit(0);
 		}
 		// empty cars.dat file, this error should be ignored
 		catch (java.io.IOException exp)
 		{
-			System.out.println("The data file, 'cars.dat' is possibly corrupted. Please delete it and create a new empty data file named cars.dat");
+			logger.log(Level.WARNING, "The data file, 'cars.dat' is possibly corrupted. Please delete it and create a new empty data file named cars.dat");
 			System.exit(0);
 		}
 		catch (Exception exp)
 		{
-			System.out.println("There was an error loading 'cars.dat'. Try deleting and creating a new empty file named 'cars.dat'");
+			logger.log(Level.WARNING, "There was an error loading 'cars.dat'. Try deleting and creating a new empty file named 'cars.dat'");
 			System.exit(0);
 		}
 
@@ -481,13 +485,13 @@ public class CarSalesSystem extends JFrame implements ActionListener, ComponentL
 			}
 			catch (NoSuchMethodException exp)
 			{
-				System.out.println("Warning, 'public carsUpdated(CarEvent)' method does not exist in " + registeredListeners.get(i).getClass().getName() + ". You will not receive any car update events");
+				logger.log(Level.WARNING, "'public carsUpdated(CarEvent)' method does not exist in " + registeredListeners.get(i).getClass().getName() + ". You will not receive any car update events");
 			}
 			catch (IllegalAccessException exp)
 			{
-				System.out.println("Warning, the 'public carUpdated(CarEvent)' method couldn't be called for unknown reasons, You will not receive any car update events");
+				logger.log(Level.WARNING, "the 'public carUpdated(CarEvent)' method couldn't be called for unknown reasons, You will not receive any car update events");
 			} catch (InvocationTargetException e) {
-				System.out.println("Warning, the 'public invoke(Object)' method couldn't be called for unknown reasons, You will not register carsUpdated event");
+				logger.log(Level.WARNING, "the 'public invoke(Object)' method couldn't be called for unknown reasons, You will not register carsUpdated event");
 			}
 		}
 	}
